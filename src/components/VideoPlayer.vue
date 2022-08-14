00b40725609a0472df0593cb086d5e3e62438291
robinsonweng <robinsonweng100@gmail.com>
2022-08-14 21:33:59 +0800

    Update process of getting stream url

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,34 +1,47 @@
 <script setup>
-import { ref, onMounted, defineProps } from "vue";
+import { ref, onBeforeMount, onMounted, defineProps } from "vue";
 import { useRoute, onBeforeRouteUpdate } from "vue-router";
 import Hls from "hls.js";
 
 const route = useRoute();
-const message = defineProps(["message"]);
-const seriesData = ref(message);
-const video_id = seriesData.value["message"][route.params.videoId - 1].uuid;
+const series = defineProps(["message"]);
+const seriesInfo = ref(series.message);
+const streamUrl = ref(null);
 
-const startPlayer = (ele, id) => {
-  let source = `https://savagetime.mooo.com:5000/hls/${id}.mp4/index.m3u8?md5=id2tPFIcl59pAG1kcorMig`;
-  console.log(id);
+const getVideoInfo = (e) => {
+  if (!seriesInfo.value) return "";
+  return seriesInfo.value.filter((t) => t.episode == e)[0].uuid;
+};
+
+const getStreamUrl = async (id) => {
+  streamUrl.value = "";
+  let url = `https://savagetime.mooo.com/api/dev/video/${id}/stream`;
+  const location = await fetch(url)
+    .then((response) => response.json());
+  return location;
+};
+
+const startPlayer = (ele, source) => {
+  let url = `https://savagetime.stream.mooo.com${source.url}`;
   if (Hls.isSupported()) {
     const hls = new Hls();
     hls.attachMedia(ele);
     hls.on(Hls.Events.MEDIA_ATTACHED, () => {
-      hls.loadSource(source);
+      hls.loadSource(url);
     });
   }
 };
 
 const player = ref();
-onMounted(() => {
-  startPlayer(player.value, video_id);
+
+onMounted(async () => {
+  const vId = getVideoInfo(route.params.videoId);
+  startPlayer(player.value, await getStreamUrl(vId));
 });
 
-onBeforeRouteUpdate((to, from) => {
-  console.log(to.params.videoId);
-  const video_id = seriesData.value["message"][to.params.videoId - 1].uuid;
-  startPlayer(player.value, video_id);
+onBeforeRouteUpdate(async (to, from) => {
+  const vId = getVideoInfo(to.params.videoId);
+  startPlayer(player.value, await getStreamUrl(vId));
 });
 </script>
 
