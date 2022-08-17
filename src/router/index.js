62cadf4ba90b27b1d4d467920461c03058c01278
robinsonweng <robinsonweng100@gmail.com>
2022-08-17 16:46:10 +0800

    Change history to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
@@ -13,7 +13,7 @@ const router = createRouter({
       path: "/series/:seriesId",
       name: "series",
       component: () => import("../views/SeriesView.vue"),
-      meta: { title: "Redirecting" }
+      meta: { title: "Redirecting" },
     },
     {
       path: "/video/:videoId",
